f6c23d12c795d605a81d2116213318eb38977095
dylanleclair <dylan.leclair@icloud.com>
2025-07-09 20:44:57 -0600

    last try 2

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://dylanleclair.github.io/portfolio/deploying-vite-project-example/",
+  base: "https://dylanleclair.github.io/portfolio/",
 })
